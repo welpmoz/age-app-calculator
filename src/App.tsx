4fa7dbd8b0b110718311f401bdb67dd503e0f2dc
roswell <roswell_pando@outlook.es>
2023-05-27 10:06:32 -0500

    agregué la estructura de la pagina web

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,45 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      Esta es la aplicacion
-    </div>
+    <>
+      <form>
+        <div>
+          <span>day</span>
+          <input type="text" />
+        </div>
+        <div>
+          <span>month</span>
+          <input type="text" />
+        </div>
+        <div>
+          <span>year</span>
+          <input type="text" />
+        </div>
+      </form>
+
+      <div>
+        <hr />
+        <button>Mi icono</button>
+      </div>
+
+      <div>
+        <div>
+          <p>
+            <span>--</span> years
+          </p>
+        </div>
+        <div>
+          <p>
+            <span>--</span> months
+          </p>
+        </div>
+        <div>
+          <p>
+            <span>--</span> days
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
 
